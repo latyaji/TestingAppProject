05e7d21fd564c7c1100c45d59d2c8cd3ee09a34b
latyaji <shivani.latya@taskcraft.in>
2023-08-21 17:17:01 +0530

    now i will start redux

--- src/screen/signup/Signup.tsx
+++ src/screen/signup/Signup.tsx
@@ -1,125 +1,300 @@
+// import { View, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native'
+// import React, { useState } from 'react'
+// import { styles } from '../../globalstyle/Styles'
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+// export const Signup = ({ navigation }) => {
+//   const [allfielddata, setAllfielddata] = useState({
+//     name: '',
+//     email: '',
+//     phone: '',
+//     password: '',
+//     confirmpassword: ''
+//   })
+
+//   const [allfielderror, setAllfielderror] = useState({
+//     nameerror: '',
+//     emailerror: '',
+//     phoneerror: '',
+//     passworderror: '',
+//     confirmpassworderror: ''
+//   })
+
+//   const [showpassword, setShowpassword] = useState(true)
+
+//   const handleallerrors = (text, fieldname) => {
+//     let allerrors = {}
+//     if (fieldname == 'name') {
+//       const charcter = /[^a-z]/gi
+//       { allerrors.nameerror = !text ? "Name Field is required" : null }
+//       if (text) {
+//         { allerrors.nameerror = charcter.test(text) ? "numeric character not allowed" : null }
+//       }
+//     }
+//     else if (fieldname == 'email') {
+//       const emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//       { allerrors.emailerror = !text ? "Email Field is required" : null }
+//       if (text) {
+//         { allerrors.emailerror = !emailregex.test(text) ? "Email must be valid" : null }
+//       }
+//     }
+//     else if (fieldname == 'phone') {
+//       { allerrors.phoneerror = !text ? "Phone Field is required" : null }
+//       if (text) {
+//         { text.length != 10 ? allerrors.phoneerror = "Phone Field should be 10 character" : null }
+//       }
+//     }
+//     else if (fieldname == 'password') {
+//       const passwordregex = /^.{6}$/;
+//       { allerrors.passworderror = !text ? "Password Field is required" : null }
+//       if (text) {
+//         { allerrors.passworderror = !passwordregex.test(text) ? "Password should be only 6 chanracter" : null }
+//       }
+//     }
+//     else if (fieldname == 'confirmpassword') {
+//       { allerrors.confirmpassworderror = !text ? "Confirm Password Field is required" : null }
+//       if (text) {
+//         { allerrors.confirmpassworderror = text !== allfielddata.password ? "Confirm Password not matched with password" : null }
+//       }
+//     }
+//     return allerrors
+//   }
+
+//   const handleInput = (text, fieldname) => {
+//     const isValid = handleallerrors(text, fieldname)
+
+//     setAllfielderror({ ...allfielderror, ...isValid })
+//     setAllfielddata({ ...allfielddata, [fieldname]: text })
+//   }
+
+//   const handleSignupbtn = async () => {
+//     let submittederror = {}
+//     let errorset = false
+
+//     for (let i in allfielddata) {
+//       const checkallfield = handleallerrors(allfielddata[i], i)
+//       submittederror = { ...submittederror, ...checkallfield }
+//       if (!allfielddata[i]) {
+//         errorset = true
+//       }
+//     }
+//     setAllfielderror({ ...allfielderror, ...submittederror })
+
+//     if (!errorset) {
+//       try {
+//         //  await AsyncStorage.clear();
+//         const savedUserJSON = await AsyncStorage.getItem('user');
+//         if (savedUserJSON) {
+//           const getdata = JSON.parse(savedUserJSON) // ...[{},{},{}] // {},{},{}   
+//          // console.log("getdata", getdata);
+//           await AsyncStorage.setItem('user', JSON.stringify([...getdata, allfielddata]));    //[...[{},{},{}],allfielddata] ,//[{},{},{},{}]
+//         }
+//         else {
+//           await AsyncStorage.setItem('user', JSON.stringify([allfielddata]));
+//         }
+//         setAllfielddata({
+//           name: '',
+//           email: '',
+//           phone: '',
+//           password: '',
+//           confirmpassword: ''
+//         });
+//         // Clear field errors
+//         setAllfielderror({
+//           nameerror: '',
+//           emailerror: '',
+//           phoneerror: '',
+//           passworderror: '',
+//           confirmpassworderror: ''
+//         });
+
+//         navigation.navigate('Login');
+
+//       } catch (error) {
+//         console.error('Error saving user data:', error);
+//       }
+//     }
+//   }
+
+//   return (
+// <ScrollView>
+//   <Text style={styles.headertxt}>Sign Up Form</Text>
+//   <Text style={styles.labeltxt}>Enter name</Text>
+//   <TextInput
+//     placeholder='Enter Your name'
+//     style={styles.inputfield}
+//     onChangeText={(text) => handleInput(text, fieldname = "name")}
+//     defaultValue={allfielddata.name}
+//   />
+//   {allfielderror.nameerror ? <Text style={styles.errortxt}>{allfielderror.nameerror}</Text> : null}
+
+//   <Text style={styles.labeltxt}>Enter Email</Text>
+//   <TextInput
+//     placeholder='Enter Your Email'
+//     style={styles.inputfield}
+//     onChangeText={(text) => handleInput(text, fieldname = "email")}
+//     defaultValue={allfielddata.email}
+//   />
+//   {allfielderror.emailerror ? <Text style={styles.errortxt}>{allfielderror.emailerror}</Text> : null}
+
+//   <Text style={styles.labeltxt}>Enter Phone </Text>
+//   <TextInput
+//     placeholder='Enter Your Phone'
+//     maxLength={10}
+//     style={styles.inputfield}
+//     keyboardType={'phone-pad'}
+//     defaultValue={allfielddata.phone}
+//     onChangeText={(text) => handleInput(text, fieldname = "phone")}
+//   />
+//   {allfielderror.phoneerror ? <Text style={styles.errortxt}>{allfielderror.phoneerror}</Text> : null}
+
+//   <Text style={styles.labeltxt}>Enter Password</Text>
+//   <View
+//     style={styles.passwordfield}>
+//     <TextInput
+//       placeholder='Enter Your Password'
+//       secureTextEntry={showpassword}
+//       defaultValue={allfielddata.password}
+//       onChangeText={(text) => handleInput(text, fieldname = "password")}
+//     />
+//     <TouchableOpacity
+//       onPress={() => setShowpassword(!showpassword)}
+//     >
+//       <Text style={styles.showtxt}>Show</Text>
+//     </TouchableOpacity>
+//   </View>
+
+//   {allfielderror.passworderror ? <Text style={styles.errortxt}>{allfielderror.passworderror}</Text> : null}
+
+//   <Text style={styles.labeltxt}>Enter Confirm Password</Text>
+
+//   <View
+//     style={styles.passwordfield}>
+//     <TextInput
+//       placeholder='Enter Your Confirm Password'
+//       secureTextEntry={showpassword}
+//       defaultValue={allfielddata.confirmpassword}
+//       onChangeText={(text) => handleInput(text, fieldname = "confirmpassword")}
+//     />
+//     <TouchableOpacity
+//       onPress={() => setShowpassword(!showpassword)}
+//     >
+//       <Text style={styles.showtxt}>Show</Text>
+//     </TouchableOpacity>
+//   </View>
+//   {allfielderror.confirmpassworderror ? <Text style={styles.errortxt}>{allfielderror.confirmpassworderror}</Text> : null}
+//   <TouchableOpacity
+//     onPress={handleSignupbtn}
+//     style={styles.buttoncontainer}
+//   >
+//     <Text style={styles.buttontxt}>Sign Up</Text>
+//   </TouchableOpacity>
+// </ScrollView>
+
+//   )
+// }
+
+
+
 import { View, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { styles } from '../../globalstyle/Styles'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
-export const Signup = ({ navigation }) => {
+
+export const Signup = () => {
+  const [passwordvisible, setPasswordvisible] = useState(true)
+
   const [allfielddata, setAllfielddata] = useState({
     name: '',
     email: '',
     phone: '',
     password: '',
-    confirmpassword: ''
+    cpassword: ''
   })
 
-  const [allfielderror, setAllfielderror] = useState({
+  const [allfilederror, setAllfielderror] = useState({
     nameerror: '',
     emailerror: '',
     phoneerror: '',
     passworderror: '',
-    confirmpassworderror: ''
+    cpassworderror: ''
   })
 
-  const [showpassword, setShowpassword] = useState(true)
-
-  const handleallerrors = (text, fieldname) => {
-    let allerrors = {}
-    if (fieldname == 'name') {
+  const handleerrors = (text, filedname) => {
+    const errorset = {}
+    if (filedname == 'name') {
       const charcter = /[^a-z]/gi
-      { allerrors.nameerror = !text ? "Name Field is required" : null }
+      { errorset.nameerror = !text ? 'Name Field is Required' : null }
       if (text) {
-        { allerrors.nameerror = charcter.test(text) ? "numeric character not allowed" : null }
+        { errorset.nameerror = charcter.test(text) ? "Numeric letter not allowed" : null }
       }
     }
-
-    else if (fieldname == 'email') {
+    else if (filedname == 'email') {
       const emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      { allerrors.emailerror = !text ? "Email Field is required" : null }
+      { errorset.emailerror = !text ? 'Email Field is Required' : null }
       if (text) {
-        { allerrors.emailerror = !emailregex.test(text) ? "Email must be valid" : null }
+        { errorset.emailerror = !emailregex.test(text) ? 'Email not valid' : null }
+
       }
     }
-    else if (fieldname == 'phone') {
-      { allerrors.phoneerror = !text ? "Phone Field is required" : null }
+    else if (filedname == 'phone') {
+      { errorset.phoneerror = !text ? 'Phone Field is Required' : null }
+    }
+    else if (filedname == 'password') {
+      { errorset.passworderror = !text ? 'Password Field is Required' : null }
+    }
+    else if (filedname == 'cpassword') {
+      { errorset.cpassworderror = !text ? 'Confirm password Field is Required' : null }
       if (text) {
-        { text.length != 10 ? allerrors.phoneerror = "Phone Field should be 10 character" : null }
+        { errorset.cpassworderror = allfielddata.password != text ? 'Confirm password not match' : null }
+
       }
     }
-    else if (fieldname == 'password') {
-      const passwordregex = /^.{6}$/;
-      { allerrors.passworderror = !text ? "Password Field is required" : null }
-      if (text) {
-        { allerrors.passworderror = !passwordregex.test(text) ? "Password should be only 6 chanracter" : null }
-      }
-    }
-    else if (fieldname == 'confirmpassword') {
-      { allerrors.confirmpassworderror = !text ? "Confirm Password Field is required" : null }
-      if (text) {
-        { allerrors.confirmpassworderror = text !== allfielddata.password ? "Confirm Password not matched with password" : null }
-      }
-    }
-    return allerrors
+    return errorset
   }
 
-  const handleInput = (text, fieldname) => {
-    const isValid = handleallerrors(text, fieldname)
-
-    setAllfielderror({ ...allfielderror, ...isValid })
-    setAllfielddata({ ...allfielddata, [fieldname]: text })
+  const handleInput = (text, filedname) => {
+    const isValid = handleerrors(text, filedname)
+    setAllfielderror({ ...allfilederror, ...isValid })
+    setAllfielddata({ ...allfielddata, [filedname]: text })
   }
 
-  const handleSignupbtn = async () => {
-    let submittederror = {}
-    let errorset = false
+  const handleSubmit = async () => {
+    let submiterror = {}
+    let seterror = false
 
     for (let i in allfielddata) {
-      const checkallfield = handleallerrors(allfielddata[i], i)
-      submittederror = { ...submittederror, ...checkallfield }
+      const checkallfielderror = handleerrors(allfielddata[i], i)
+      submiterror = { ...submiterror, ...checkallfielderror }
       if (!allfielddata[i]) {
-        errorset = true
+        seterror = true
       }
     }
-    setAllfielderror({ ...allfielderror, ...submittederror })
-
-    if (!errorset) {
+    setAllfielderror({ ...allfilederror, ...submiterror })
+    if (!seterror) {
+      // console.log("data submitted",allfielddata);
       try {
-        //  await AsyncStorage.clear();
-        const savedUserJSON = await AsyncStorage.getItem('user');
-        if (savedUserJSON) {
-          const getdata = JSON.parse(savedUserJSON) // ...[{},{},{}] // {},{},{}
-          //[...[{},{},{}],allfielddata] ,//[{},{},{},{}]
-          await AsyncStorage.setItem('user', JSON.stringify([...getdata, allfielddata]));
+        const storedata = await AsyncStorage.getItem('formdata')
+        if (storedata) {
+          const keepnewdata = JSON.parse(storedata)          
+          await AsyncStorage.setItem('formdata', JSON.stringify([...keepnewdata, allfielddata]))
+
         }
         else {
-          await AsyncStorage.setItem('user', JSON.stringify([allfielddata]));
+          const savedata = await AsyncStorage.setItem('formdata', JSON.stringify([allfielddata]))
+          console.log("savedata",savedata);
+          
         }
-        const alldata = await AsyncStorage.getItem('user');
-        console.log("allldata", alldata);
-
-        setAllfielddata({
-          name: '',
-          email: '',
-          phone: '',
-          password: '',
-          confirmpassword: ''
-        });
-
-        // Clear field errors
-        setAllfielderror({
-          nameerror: '',
-          emailerror: '',
-          phoneerror: '',
-          passworderror: '',
-          confirmpassworderror: ''
-        });
-
-        navigation.navigate('Login');
 
       } catch (error) {
-        console.error('Error saving user data:', error);
+        console.log("error", error);
+
       }
     }
+
   }
 
   return (
@@ -129,20 +304,19 @@ export const Signup = ({ navigation }) => {
       <TextInput
         placeholder='Enter Your name'
         style={styles.inputfield}
-        onChangeText={(text) => handleInput(text, fieldname = "name")}
         defaultValue={allfielddata.name}
+        onChangeText={(text) => handleInput(text, filedname = "name")}
       />
-      {allfielderror.nameerror ? <Text style={styles.errortxt}>{allfielderror.nameerror}</Text> : null}
+      {allfilederror.nameerror ? <Text style={styles.errortxt}>{allfilederror.nameerror}</Text> : null}
 
       <Text style={styles.labeltxt}>Enter Email</Text>
       <TextInput
         placeholder='Enter Your Email'
         style={styles.inputfield}
-        onChangeText={(text) => handleInput(text, fieldname = "email")}
         defaultValue={allfielddata.email}
+        onChangeText={(text) => handleInput(text, filedname = "email")}
       />
-      {allfielderror.emailerror ? <Text style={styles.errortxt}>{allfielderror.emailerror}</Text> : null}
-
+      {allfilederror.emailerror ? <Text style={styles.errortxt}>{allfilederror.emailerror}</Text> : null}
       <Text style={styles.labeltxt}>Enter Phone </Text>
       <TextInput
         placeholder='Enter Your Phone'
@@ -150,27 +324,29 @@ export const Signup = ({ navigation }) => {
         style={styles.inputfield}
         keyboardType={'phone-pad'}
         defaultValue={allfielddata.phone}
-        onChangeText={(text) => handleInput(text, fieldname = "phone")}
+        onChangeText={(text) => handleInput(text, filedname = "phone")}
+
       />
-      {allfielderror.phoneerror ? <Text style={styles.errortxt}>{allfielderror.phoneerror}</Text> : null}
+      {allfilederror.phoneerror ? <Text style={styles.errortxt}>{allfilederror.phoneerror}</Text> : null}
 
       <Text style={styles.labeltxt}>Enter Password</Text>
       <View
         style={styles.passwordfield}>
         <TextInput
           placeholder='Enter Your Password'
-          secureTextEntry={showpassword}
+          secureTextEntry={passwordvisible}
           defaultValue={allfielddata.password}
-          onChangeText={(text) => handleInput(text, fieldname = "password")}
+          onChangeText={(text) => handleInput(text, filedname = "password")}
+
         />
+
         <TouchableOpacity
-          onPress={() => setShowpassword(!showpassword)}
+          onPress={() => setPasswordvisible(!passwordvisible)}
         >
           <Text style={styles.showtxt}>Show</Text>
         </TouchableOpacity>
       </View>
-
-      {allfielderror.passworderror ? <Text style={styles.errortxt}>{allfielderror.passworderror}</Text> : null}
+      {allfilederror.passworderror ? <Text style={styles.errortxt}>{allfilederror.passworderror}</Text> : null}
 
       <Text style={styles.labeltxt}>Enter Confirm Password</Text>
 
@@ -178,27 +354,30 @@ export const Signup = ({ navigation }) => {
         style={styles.passwordfield}>
         <TextInput
           placeholder='Enter Your Confirm Password'
-          secureTextEntry={showpassword}
-          defaultValue={allfielddata.confirmpassword}
-          onChangeText={(text) => handleInput(text, fieldname = "confirmpassword")}
+          secureTextEntry={passwordvisible}
+          defaultValue={allfielddata.cpassword}
+          onChangeText={(text) => handleInput(text, filedname = "cpassword")}
         />
         <TouchableOpacity
-          onPress={() => setShowpassword(!showpassword)}
+          onPress={() => setPasswordvisible(!passwordvisible)}
         >
           <Text style={styles.showtxt}>Show</Text>
         </TouchableOpacity>
       </View>
-      {allfielderror.confirmpassworderror ? <Text style={styles.errortxt}>{allfielderror.confirmpassworderror}</Text> : null}
+      {allfilederror.cpassworderror ? <Text style={styles.errortxt}>{allfilederror.cpassworderror}</Text> : null}
+
+
       <TouchableOpacity
-        onPress={handleSignupbtn}
         style={styles.buttoncontainer}
+        onPress={handleSubmit}
       >
         <Text style={styles.buttontxt}>Sign Up</Text>
       </TouchableOpacity>
     </ScrollView>
-
   )
 }
+
+////
 
 
 
